689d9f44d14985f57a534308521e35e2a8bfa234
Quan Tran <minhquantran451@gmail.com>
2022-10-04 18:57:34 +0700

    initial

---
+++ src/lib/FeedbackList.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { FeedbackStore } from "../stores";
+  import { scale, fade } from "svelte/transition";
+  import FeedbackItem from "./FeedbackItem.svelte";
+</script>
+
+{#each $FeedbackStore as feadback (feadback.id)}
+  <div in:fade out:scale>
+    <FeedbackItem item={feadback} />
+  </div>
+{/each}
